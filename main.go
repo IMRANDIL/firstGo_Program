49d06fe5c8cd4a6eb5d92b6aae05431aad15f576
IMRANDIL <aliimranadil2@gmail.com>
2023-01-03 21:35:58 +0530

    first go program

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+//package main means ...it will create a executable file for us....
+// if package main...then func main required...
+
+import "fmt"
+
+
+
+
+func main(){
+	fmt.Println("hey ali what is up man!")
+}

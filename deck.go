1c3f440422c0674437e2d11ab09533ae05ff530b
IMRANDIL <aliimranadil2@gmail.com>
2023-01-04 07:46:46 +0530

    some more chunk

--- deck.go
+++ deck.go
@@ -9,6 +9,22 @@ import "fmt"
 type deck []string
 
 
+func newDec() deck{
+	cards := deck{}
+
+	cardSuits := []string{"Spades","Diamonds","Hearts","Clubs"}
+	cardValues := []string{"Ace","Two","Three","Four"}
+
+	for _,suits := range cardSuits{
+		for _, values := range cardValues {
+			cards = append(cards, suits+" of "+values)
+		}
+	}
+	return cards
+}
+
+
+
 func (d deck) print(){
 	for i, card := range d {
 		fmt.Println(i,card)

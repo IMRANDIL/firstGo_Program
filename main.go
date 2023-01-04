29be07db424573ec160d58bffffe3e897837a41b
IMRANDIL <aliimranadil2@gmail.com>
2023-01-04 07:52:29 +0530

    did it

--- main.go
+++ main.go
@@ -7,9 +7,7 @@ package main
 
 
 func main(){
-	cards := deck{"Ace of Diamonds",newCard()}
-
-	cards = append(cards, "Six of Spades")
+	cards := newDec()
 
 cards.print()
 
@@ -17,6 +15,3 @@ cards.print()
 }
 
 
-func newCard() string{
-	return "The Ace of Spades"
-}
